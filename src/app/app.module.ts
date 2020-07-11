fc8926f704c4bf8c9b165e365f447e216b37d0ae
Tania Choudhury <hchoudhurytania@gmail.com>
2020-07-11 20:56:23 +0530

    all spec files are deleted as no test has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookDisplayComponent } from './book-display/book-display.component';
-import { ActionBtnDirective } from './action-btn.directive';
 import { BookViewComponent } from './book-view/book-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookDisplayComponent,
-    ActionBtnDirective,
     BookViewComponent
   ],
   imports: [
